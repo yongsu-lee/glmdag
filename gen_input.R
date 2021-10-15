978cfd0f11631fcd28b85ca54d75c7a5f7801f21
Yongsu Lee <yongsulee@stat.wisc.edu>
2021-10-15 03:31:33 -0500

    add mmhc_script.R

--- gen_input.R
+++ gen_input.R
@@ -1,17 +1,16 @@
 ## Generate Simulation Information ####
 simu_info = list(
-  simu_case = "simu2",
-  size = "large",
+  simu_case = "mmhc",
+  size = "small",
   seed_para = 1,
   n_obs = 50,
   n_multi = 4,
-  n_conti = 4,
-  n_ordin = 4,
-  n_lams = 3,
-  eps_lam = 0.4,
-  path_par = TRUE
+  n_conti = 3,
+  n_ordin = 3,
+  n_lams = 30,
+  eps_lam = 0.05,
+  path_par = FALSE
 )
-
 saveRDS(simu_info, "simu_info.rds")
 
 ## Generate queue list ####
@@ -23,5 +22,5 @@ queue_list = function(args_list, file_name){
               quote = F, row.names = F, col.names = F)
 }
 
-args_list = list(c("tree"), c("mc","mo","mm"), 1:50, 1:30)
+args_list = list(c("bi", "rand", "sf", "sw", "tree" ), c("mc", "mo", "mm"), 1:100)
 queue_list(args_list, "queue_list")
---
+++ mmhc_script.R
@@ -0,0 +1,65 @@
+# Randomly assign continuous or multinomial nodes
+types_by_node = gen_node_types(n_conti + n_ordin, n_multi, 0, seed = seed_para)
+
+# Generate the number of levels for each node randomly 
+n_levels_by_node = gen_node_levels(types_by_node, 4, seed = seed_para)
+#.. it would be 1 if the node is continuous  
+
+# Generate the grand parameter matrix W 
+W_true = gen_para(A_true, types_by_node, n_levels_by_node, seed = seed_para)
+
+# Generate datapoints
+data_input = gen_data(n_obs, A_true, graph_true, W_true, seed = iter)
+#.. Note that the seed set with the 'iter' number
+
+if (size == "small"){
+  
+  ## Discretizing continuous nodes either 2 or 4 levels
+  conti_nodes = which(types_by_node == "c")
+  two_level_nodes = conti_nodes[1:(n_conti/2)]
+  four_level_nodes = conti_nodes[(n_conti/2+1):n_conti]
+  
+} else if (size == "large") {
+  
+  ## Discretizing continuous nodes either 2 or 4 levels
+  
+  # Select will-be discretizing nodes (20 nodes)
+  disc_nodes = sample(which(types_by_node == "c"), n_ordin, replace = F)
+  n_disc = length(disc_nodes)
+  conti_raw_nodes = which(types_by_node == "c")
+  conti_nodes = setdiff(conti_raw_nodes, disc_nodes) 
+  two_level_nodes = disc_nodes[1:(n_disc/2)]
+  four_level_nodes = disc_nodes[(n_disc/2+1):n_disc]
+  
+}
+
+# Discretizing into 2 levels
+temp = data_input[two_level_nodes]
+temp1 = lapply(temp, function(x) 
+  cut(x, c(min(x)-1, median(x), max(x)), labels = 1:2))
+
+# Discretizing into 4 levels
+temp = data_input[four_level_nodes]
+temp2 = lapply(temp, function(x) 
+  cut(x, c(min(x)-1, quantile(as.matrix(x), probs = c(0.25, 0.5, 0.75, 1))), 
+      labels = 1:4))
+
+discretized_data = data.frame(temp1, temp2)
+
+## Generate a different type of dataset according to the method
+data_input = 
+  switch(method,
+         # Oringinal data
+         "mc" = { data_input },
+         # Consider discretized nodes as 'nominal-level' nodes
+         "mm" = { data_input[conti_nodes] <- discretized_data; data_input},
+         # Consider discretized nodes as 'ordinal-level' nodes
+         "mo" = {
+           ordered = lapply(discretized_data, function(x) factor(x, ordered = T))
+           data_input[conti_nodes] <- ordered; data_input
+         } )
+
+## Run main functions
+A_est_hc = hc(data_input)
+A_est_mmhc = mmhc(data_input)
+result = list(hc = A_est_hc, mmhc = A_est_mmhc)
