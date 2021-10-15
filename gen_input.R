5452c804d0eccb68e97ed8c29859ad85c53f738b
Yongsu Lee <yongsulee@stat.wisc.edu>
2021-10-15 02:15:09 -0500

    master_script.R modified

--- gen_input.R
+++ gen_input.R
@@ -1,32 +1,27 @@
 ## Generate Simulation Information ####
 simu_info = list(
   simu_case = "simu2",
-  size = "small",
-  method = "mc",
+  size = "large",
   seed_para = 1,
   n_obs = 50,
-  n_multi <- 4,
-  n_conti <- 3,
-  n_ordin <- 3,
-  n_nodes = n_multi + n_conti + n_ordin,
+  n_multi = 4,
+  n_conti = 4,
+  n_ordin = 4,
   n_lams = 3,
-  eps_lam = 0.3,
-  path_par = FALSE
+  eps_lam = 0.4,
+  path_par = TRUE
 )
 
-saveRDS(simu_info, "simu_info_test.rds")
+saveRDS(simu_info, "simu_info.rds")
 
 ## Generate queue list ####
-queue_list = function(args_list){
+queue_list = function(args_list, file_name){
   
   n_args = length(args_list)
   combn = expand.grid(args_list)
-  write.table(combn, file = "queue_list", sep=", ", 
+  write.table(combn, file = file_name, sep=", ", 
               quote = F, row.names = F, col.names = F)
 }
 
 args_list = list(c("tree"), c("mc","mo","mm"), 1:50, 1:30)
-
-# args_list = list(1:20, c("alarm"), c("FALSE"))
-
-queue_list(args_list)
+queue_list(args_list, "queue_list")
--- master_script.R
+++ master_script.R
@@ -16,20 +16,21 @@ if (sysname == "Linux"){ # for CHTC server
   #!/usr/bin/env Rscript
   queue_args = commandArgs(trailingOnly=TRUE)
   graph_type = as.character(queue_args[1])
-  # method = as.character(queue_args[2])
-  iter = as.integer(queue_args[2])
-  ell = NULL
+  method = as.character(queue_args[2])
+  iter = as.integer(queue_args[3])
+  ell = as.integer(queue_args[4])
   
-  name_temp = paste(simu_case, size,"n", n_obs, graph_tlsype, iter, sep = "_")
+  name_temp = paste(simu_case, size,"n", n_obs, 
+  	            graph_type, method, iter, ell, sep = "_")
   # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  
+
   # Pull glmdag codes from github
   url = "https://github.com/yongsu-lee/glmdag/archive/refs/heads/master.zip"
   download.file(url = url, destfile = "glmdag.zip")
   Sys.sleep(3)
   unzip("glmdag.zip")
   init_dir = "./glmdag-master/"
-  
+
 } else { # for local macOS
   
   setwd("~/iCloud/glmdag/")
@@ -45,20 +46,22 @@ if (sysname == "Linux"){ # for CHTC server
   
   name_temp = paste(simu_case, size,"n", n_obs, graph_type, iter, sep = "_")
   #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  
+
 }
+
 
 ## Sourcing files
 source(paste0(init_dir,"codes/00_load_ftns.R"))
 
 ## Generate the true graph and obtain corresponding adjacency matrix
+n_nodes = n_conti + n_multi + n_ordin
 graph_set = gen_graph_adj_mat(n_nodes, graph_type, seed = seed_para)
 graph_true = graph_set$graph_true
 A_true = graph_set$A_true
 n_edge = sum(A_true) 
 
 ## Run simulation
-source(paste0(init_dir, simu_case, "_script.R"))
+source(paste0(init_dir,simu_case,"_script.R"))
 
 ## Save the result
 saveRDS(result, file = paste0(name_temp,".rds"))
