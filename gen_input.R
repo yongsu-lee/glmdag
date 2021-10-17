bf15a8cf9cd201d8bfa0d217cc3d7fa5f6973ba1
Yongsu Lee <yongsulee@stat.wisc.edu>
2021-10-17 05:23:57 -0500

    remove simu_info

--- gen_input.R
+++ gen_input.R
@@ -1,6 +1,6 @@
 ## Generate Simulation Information ####
 simu_info = list(
-  simu_case = "simu2",
+  simu_case = "mmhc",
   size = "large",
   seed_para = 1,
   n_obs = 200,
@@ -8,7 +8,7 @@ simu_info = list(
   n_conti = 15,
   n_ordin = 20,
   n_lams = 30,
-  eps_lam = 0.4,
+  eps_lam = 0.05,
   path_par = TRUE
 )
 saveRDS(simu_info, "simu_info.rds")
@@ -22,5 +22,5 @@ queue_list = function(args_list, file_name){
               quote = F, row.names = F, col.names = F)
 }
 
-args_list = list("rand", c("mc", "mo", "mm"), 1:50, 1:30)
+args_list = list("rand", c("mc", "mo", "mm"), 1:50)
 queue_list(args_list, "queue_list")
